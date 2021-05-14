ff9a69c5e073023e2417fafdb3db82a6190fc91f
Ahmad Ali <ahmad.abdulmuttaleb@gmail.com>
2021-05-14 13:18:56 +0200

    adding fonts, basic structure for screen with name, profession, phone number and email.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,96 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  foregroundImage: AssetImage('assets/images/my_avatar.jpg'),
+                ),
+                Text(
+                  'Ahmad AbdulMuttaleb',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'
+                  ),
+                ),
+                Text(
+                  'Software Engineer',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+2 010 0807 0689',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source Sans Serif',
+                          fontSize: 20.0
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5.0,),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(width: 10.0,),
+                      Flexible(
+                        child: Text(
+                          'ahmad.abdulmuttaleb@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal[900],
+                            fontFamily: 'Source Sans Serif',
+                            fontSize: 20.0,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ),
       ),
     ),
   );
